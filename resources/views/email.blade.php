440dddff5061c26f16d732c0f1634c5bf1cf92f6
damien <damien@anaximandre.com>
2025-03-25 15:02:11 +0100

    chore: view

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,9 +1,5 @@
 {!! $blueprint->getData()['message'] !!}
 
-<a href="{{ $blueprint->getData()['url'] }}">Click to access</a>
-
---
-Best regards
 
 
 If you  wish to unsubscribe, you can go to the forum's settings page and uncheck "Custom notifications" : https://cop.nutri-know.eu
